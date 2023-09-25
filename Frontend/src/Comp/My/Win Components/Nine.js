b7dfa463d59485744de5ac1c0050e8540c6bb7cf
Navneet Singh <navneetsingh191302108@gmail.com>
2023-09-25 17:09:03 +0530

    Added zero to nine components

---
+++ Frontend/src/Comp/My/Win Components/Nine.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal'
+
+
+
+const Nine = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    return (
+        <div>
+            {/* <Button variant="success" className='btn' onClick={handleShow} >
+                Select Zero
+            </Button> */}
+            <Button data-v-3acbc54d="" onClick={handleShow} className="btn btn-success" data-num="0">9</Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header className='bg-dark text-light' closeButton>
+                    <Modal.Title className='fs-5 text-center'>Select 9</Modal.Title>
+                </Modal.Header>
+                <Modal.Body> 
+
+
+                </Modal.Body>
+                <Modal.Footer >
+                    <Button variant="dark" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="dark" onClick={handleClose}>
+                        Confirm
+                    </Button>
+                   
+
+
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
+
+export default Nine
